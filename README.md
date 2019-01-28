350094a01abe468a2c9bf1088b77d01af277ac7f
Vic Woeltjen <vic@thebeetoken.com>
2019-01-28 11:52:42 -0800

    Check in Uniswap contracts

---
+++ README.md
@@ -0,0 +1,2 @@
+Solidity interfaces and ABIs for [Uniswap](https://uniswap.io),
+as an [`npm`](http://npmjs.com) package.
---
+++ contracts/Uniswap.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.5.2;
+
+import './UniswapFactoryInterface.sol';
+import './UniswapExchangeInterface.sol';
